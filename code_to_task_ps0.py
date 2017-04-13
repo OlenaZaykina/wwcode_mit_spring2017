4f492d1533de40e820a811646051586dfddb13ec
OlenaZaykina <olenazaikina3@gmail.com>
2017-04-13 16:30:05 +0300

    This is the result to the first task Problem Set 0. A Very Simple Program: Raising a number to a power and taking a logarithm

---
+++ code_to_task_ps0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 21:30:46 2017
+
+@author: Oleksandr
+"""
+import math
+c=x**y
+x = int(input('Enter number x: '))
+y = int(input('Enter number y: '))
+d= round(math.log(x))
+print ("X**y =", c)
+print ("log(x) =", d)
+input
